f3019647b790e4aa5c66c531c9d3c0abd2574850
notarnava <71499390+arnavasoni@users.noreply.github.com>
2025-11-04 15:59:05 +0530

    Update awb_data_ext.py

--- awb_data_ext.py
+++ awb_data_ext.py
@@ -152,7 +152,7 @@ def build_prompt():
          - Return ONLY JSON, no markdown, no explanations."""
         ),
         ("human", 
-         """Extract structured data from this AWB document:\n\n\n\n
+         """Extract structured data from this AWB document:\n\n{awb_text}\n\n
          Return ONLY the JSON using this schema:
          {{
             "shipper_name": "",
@@ -231,5 +231,6 @@ if __name__ == "__main__":
     # extract_awb(r"C:\Users\SONIARN\Desktop\EXIM Sample Docs\MBAG_Prod_Parts_AWB.pdf")
     extract_awb(r"C:\Users\SONIARN\Desktop\EXIM Sample Docs\MBUSI_AWB.PDF")
     # extract_awb(r"C:\Users\SONIARN\Desktop\EXIM Sample Docs\EQS_awb.PDF")
+
 
 
